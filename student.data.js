311a7dec986e56736fdf68cfdbde47c0503f7db7
Abhishek Raj <rai.abhishekraj@gmail.com>
2021-09-21 22:02:32 +0530

    Add files via upload

---
+++ student.data.js
@@ -0,0 +1,60 @@
+// class - 8,9
+// date format: mm-dd-yyyy
+// 10 students data
+// class 8 - 3
+// class 9 - 7
+
+
+
+
+let studentData = [
+    {
+        name: "Ram",
+        rollNumber: 1,
+        marks: {
+            maths: 88,
+            engligh: 67,
+            science: 99
+        },
+        class: 8,
+        address: "Dhanbad",
+        dob: new Date('05-10-1993')
+    },
+
+    {
+        name: "Shyam",
+        rollNumber: 2,
+        marks: {
+            maths: 56,
+            engligh: 76,
+            science: 44
+        },
+        class: 8,
+        address: "Dhehradoon",
+        dob: new Date('06-11-1991')
+    },{
+        name: "Mohan",
+        rollNumber: 1,
+        marks: {
+            maths: 88,
+            engligh: 67,
+            science: 99
+        },
+        class: 8,
+        address: "Dhanbad",
+        dob: new Date('05-10-1993')
+    },
+
+    {
+        name: "Ghanshyam",
+        rollNumber: 2,
+        marks: {
+            maths: 56,
+            engligh: 76,
+            science: 44
+        },
+        class: 8,
+        address: "Dhehradoon",
+        dob: new Date('06-11-1991')
+    },
+];
